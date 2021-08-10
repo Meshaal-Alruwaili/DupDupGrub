3a73231291efce5ffcbc4595df61c7fe0f63d57e
Meshaal Alruwaili <MeshaalDev@Outlook.com>
2021-08-10 08:41:29 +0300

    Created The Project.

---
+++ DubDubGrub/DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Meshaal Hayyal on 02/01/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
