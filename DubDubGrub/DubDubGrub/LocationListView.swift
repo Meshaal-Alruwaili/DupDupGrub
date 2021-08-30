84dd875784cb87c61a7f85c2e3a731a686c1d608
Meshaal Alruwaili <MeshaalDev@Outlook.com>
2021-08-30 14:17:01 +0300

    Created Location List View

---
+++ DubDubGrub/DubDubGrub/LocationListView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationListView.swift
+//  DubDubGrub
+//
+//  Created by Meshaal Hayyal on 22/01/1443 AH.
+//
+
+import SwiftUI
+
+struct LocationListView: View {
+    var body: some View {
+        Text("Location List View!")
+    }
+}
+
+struct LocationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationListView()
+    }
+}
