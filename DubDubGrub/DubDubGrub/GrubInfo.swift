c4a803ad640ae3670b5edf2c766d7c7a58577b3d
Meshaal Alruwaili <MeshaalDev@Outlook.com>
2021-09-06 17:27:05 +0300

    Created GrubInfo File
    Nothing More to say.

---
+++ DubDubGrub/DubDubGrub/GrubInfo.swift
@@ -0,0 +1,20 @@
+//
+//  GrubInfo.swift
+//  DubDubGrub
+//
+//  Created by Meshaal Hayyal on 29/01/1443 AH.
+//
+
+import SwiftUI
+
+struct GrubInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GrubInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        GrubInfo()
+    }
+}
